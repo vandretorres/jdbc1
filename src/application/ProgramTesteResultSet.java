a148cda5f99d77905b5b598f6160e0b4cb47b9e6
vandretorres <vandre.torres@gmail.com>
2020-07-18 15:50:18 -0300

    Exemplo de inserção retornando chave primária criada automaticamente

--- src/application/ProgramTesteResultSet.java
+++ src/application/ProgramTesteResultSet.java
@@ -2,7 +2,9 @@ package application;
 
 import java.sql.Connection;
 import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.sql.Statement;
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
 
@@ -28,22 +30,39 @@ public class ProgramTesteResultSet {
 					"INSERT into seller "
 							+ "(Name, Email,BirthDate, BaseSalary,DepartmentId)"
 							+ "values "
-							+ "(?,?,?,?,?)"				
+							+ "(?,?,?,?,?)",
+							Statement.RETURN_GENERATED_KEYS
 					);
 
 
 			// substitui palceholders ( ? ) com valores que serão inseridos na base
-			st.setString(1,"Carl Purple");
-			st.setString(2,"cpurple@gmail.com");
-			st.setDate(3, new java.sql.Date(sdf.parse("14/12/1981").getTime()));
+			st.setString(1,"Mike Ross");
+			st.setString(2,"ross@gmail.com");
+			st.setDate(3, new java.sql.Date(sdf.parse("13/07/1985").getTime()));
 			st.setDouble(4, 3000.0);
 			st.setInt(5,4);
-
+			
 			
 			// Executa statement
 			int rowAffected = st.executeUpdate();
 
-			System.out.println("Done! " + rowAffected + " rows Affected");
+			if ( rowAffected > 0) {
+				
+				ResultSet rs = st.getGeneratedKeys();
+				
+				while(rs.next()) {
+					
+					int id = rs.getInt(1);
+					
+					System.out.println("Id gerado pela inserção: " + id);
+					
+				}
+				
+			}else {
+				
+				System.out.println("No rows affected");
+				
+			}
 
 		}catch (SQLException e) {
 
